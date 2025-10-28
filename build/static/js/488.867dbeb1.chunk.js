23037a2383a851b2dfc48cca8d3b04087c76eaa4
Your Name <user@example.com>
2025-10-28 22:31:01 +0100

    Update build files with BrowserRouter

--- build/static/js/488.867dbeb1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkticket_management_react=self.webpackChunkticket_management_react||[]).push([[488],{488:(e,t,a)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.867dbeb1.chunk.js.map
