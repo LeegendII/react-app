48c62a78c493f5da2762778721c8a43cf9b95373
Your Name <user@example.com>
2025-10-28 22:46:36 +0100

    Add all build files including CSS and shared-assets to gh-pages branch

---
+++ build/static/js/488.867dbeb1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkticket_management_react=self.webpackChunkticket_management_react||[]).push([[488],{488:(e,t,a)=>{new WeakMap;new Set}}]);
+//# sourceMappingURL=488.867dbeb1.chunk.js.map
